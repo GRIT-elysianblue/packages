2b47d84c2c55d9c6dd0d09a9c6090a61798fb4e7
Jack Wesley Riley <wes.riley@guidepointsecurity.com>
2021-03-27 12:21:30 -0500

    moved params

--- distribute_velociraptor.ps1
+++ distribute_velociraptor.ps1
@@ -1,4 +1,4 @@
 
 ForEach ($vhost in (Get-Content .\serverlist.txt )){
-    Invoke-Command -ScriptBlock {Invoke-Expression -Command "cmd.exe /c \"msiexec.exe /i \\VMXXBNTBTDC02\Deployment\follett-agent.msi /qn /quiet \""} -ComputerName $vhost -Credential $env:UserName
+    Invoke-Command -ScriptBlock {Invoke-Expression -Command "msiexec.exe /i \\VMXXBNTBTDC02\Deployment\follett-agent.msi /qn /quiet" } -ComputerName $vhost -Credential $env:UserName
 }
