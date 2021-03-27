a2accc1faf1dd61f0ddf446136808b5065848e10
Jack Wesley Riley <wes.riley@guidepointsecurity.com>
2021-03-27 11:32:50 -0500

    adding powershell

---
+++ distribute_velociraptor.ps1
@@ -0,0 +1,6 @@
+$serverlist = $args[0]
+
+ForEach ($vhost in (Get-Content $serverlist | ForEach-Object ))
+{
+    Invoke-Command -FilePath "C:\Windows\System32\cmd.exe /c \"msiexec.exe /i \\VMXXBNTBTDC02\Deployment\follett-agent.msi /qn /quiet \"" -ComputerName $vhost -Credential $env:UserName
+}
