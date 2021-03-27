03cd1df88bacfbbf616d44cef94a981f6d255a89
Jack Wesley Riley <wes.riley@guidepointsecurity.com>
2021-03-27 12:18:57 -0500

    moved params

--- distribute_velociraptor.ps1
+++ distribute_velociraptor.ps1
@@ -1,7 +1,4 @@
-$vparams = New-Object string[] 2
-$vparams[0] = "/c"
-$vparams[1] = "msiexec.exe /i \\VMXXBNTBTDC02\Deployment\follett-agent.msi /qn /quiet "
 
 ForEach ($vhost in (Get-Content .\serverlist.txt )){
-    Invoke-Command -FilePath "C:\Windows\System32\cmd.exe" -ArgumentList $vparams -ComputerName $vhost -Credential $env:UserName
+    Invoke-Command -ScriptBlock {Invoke-Expression -Command "cmd.exe /c \"msiexec.exe /i \\VMXXBNTBTDC02\Deployment\follett-agent.msi /qn /quiet \""} -ComputerName $vhost -Credential $env:UserName
 }
